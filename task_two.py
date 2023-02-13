ed1cb305dd23320573a521707455344fa260b572
dmitry <rbdeman@yandex.ru>
2023-02-14 00:22:46 +0300

    сделал вторую задачу

--- task_two.py
+++ task_two.py
@@ -7,3 +7,17 @@
 # Вывод: [1, 9, 13, 14, 19]
 
 
+def min_max(arr_num, min_num, max_num):
+    arr_result = []
+    for i in range(0, len(arr_num)):
+        if arr_num[i] <= max_num and arr_num[i] >= min_num:
+            arr_result.append(i)
+    print(arr_result)
+
+
+numbers = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min = int(input('введи минимальное число диапазона: '))
+max = int(input('введи аксимальное число диапазона: '))
+print(numbers)
+min_max(numbers, min, max)
+
